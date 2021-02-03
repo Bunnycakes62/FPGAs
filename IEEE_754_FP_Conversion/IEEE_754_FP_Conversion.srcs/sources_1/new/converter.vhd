7a4413d8d8ed0a32716a9590382830a4f65d28ea
Livi <piazza.olivia@gmail.com>
2021-02-02 23:24:57 -0800

    Exponential form conversion for manipulating floating point numbers of mixed precision.

---
+++ IEEE_754_FP_Conversion/IEEE_754_FP_Conversion.srcs/sources_1/new/converter.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12/02/2019 07:34:39 PM
+-- Design Name: 
+-- Module Name: converter - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity converter is
+--  Port ( );
+end converter;
+
+architecture Behavioral of converter is
+
+begin
+
+
+end Behavioral;
